a09acf8ad9d6fa363def690521a8b20b87a3a853
XeongZin <116179987+XeongZin@users.noreply.github.com>
2023-03-23 16:14:46 +0900

    Add files via upload

---
+++ Chapter07/src/sec01/ex02/MemberDAO.java
@@ -0,0 +1,65 @@
+package sec01.ex02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static final String user = "scott";
+	private static final String pwd = "tiger";
+	private Connection con;
+	//private Statement stmt;
+	private PreparedStatement pstmt;
+	
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB();
+			String query = "select * from t_member ";
+			System.out.println(query);
+			
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 靛府酒滚 肺爹 己傍");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 积己 己傍");
+//			stmt = con.createStatement();
+//			System.out.println("Statement 积己 己傍");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
